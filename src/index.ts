9d66e2dc3b854e763f6bbc804eba755cd55d6cc1
Your Name <karthiksriramoju11@gmail.com>
2025-03-15 17:55:40 +0530

    added swap logic

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-const sol_amount = 0.001;
+import { LAMPORTS_PER_SOL } from "@solana/web3.js";
+
+const sol_amount = 0.0001 * LAMPORTS_PER_SOL;
 
 import { swap } from "./swap";
 import {getTweets} from "./getTweets";
@@ -14,13 +16,13 @@ try {
     //   console.log('No tweets found.');
     //   return;
     // }
-    const tokenAddress = await getTokenFromLLM('Buy this , DezXAZ8z7PnrnRJjz3wXBoRgixCa6xjnB7YaB1pPB263');
-    if (tokenAddress!='null'){
-      await swap(tokenAddress,sol_amount);
-    }
-    else{
-      console.log('No tweets found.');
-    }
+    // const tokenAddress = await getTokenFromLLM('Buy this , DezXAZ8z7PnrnRJjz3wXBoRgixCa6xjnB7YaB1pPB263');
+    // if (tokenAddress!='null'){
+      await swap('DezXAZ8z7PnrnRJjz3wXBoRgixCa6xjnB7YaB1pPB263',sol_amount);
+    // }
+    // else{
+    //   console.log('No tweets found.');
+    // }
     
   } catch (error) {
     console.error('Error in main:', error);
--- src/swap.ts
+++ src/swap.ts
@@ -1,14 +1,85 @@
-import { Connection, Transaction, VersionedTransaction, sendAndConfirmTransaction } from '@solana/web3.js'
-import { NATIVE_MINT } from '@solana/spl-token'
+import bs58 from "bs58";
+import { Connection, Keypair, PublicKey, Transaction, VersionedTransaction } from '@solana/web3.js'
+import { NATIVE_MINT, getAssociatedTokenAddress } from '@solana/spl-token'
 import axios from 'axios'
-import { Keypair } from '@solana/web3.js'
-import bs58 from 'bs58'
 import { API_URLS } from '@raydium-io/raydium-sdk-v2'
+const isV0Tx = true;
+const connection = new Connection(process.env.RPC_URL!);
 
-const connection = new Connection('https://solana-mainnet.g.alchemy.com/v2/D747bvNsI_FXaIBlmf2lCgxcRHQu1sED')
-export const owner = Keypair.fromSecretKey(bs58.decode(process.env.PRIVATE_KEY!))
+const owner = Keypair.fromSecretKey(bs58.decode(process.env.PRIVATE_KEY!));
 
-export async function swap(tokenAddress: string,amount: number) {
-  console.log('tokenAddress',tokenAddress);
-  console.log('amount',amount);
+const slippage = 5;
+
+export async function swap(tokenAddress: string, amount: number) {
+ 
+    const { data } = await axios.get<{
+        id: string
+        success: boolean
+        data: { default: { vh: number; h: number; m: number } }
+      }>(`${API_URLS.BASE_HOST}${API_URLS.PRIORITY_FEE}`);
+
+    const { data: swapResponse } = await axios.get(
+        `${
+          API_URLS.SWAP_HOST
+        }/compute/swap-base-in?inputMint=${NATIVE_MINT}&outputMint=${tokenAddress}&amount=${amount}&slippageBps=${
+          slippage * 100}&txVersion=V0`
+    );
+
+    console.log("Swap Response Data:", swapResponse);
+    const { data: swapTransactions } = await axios.post<{
+        id: string
+        version: string
+        success: boolean
+        data: { transaction: string }[]
+      }>(`${API_URLS.SWAP_HOST}/transaction/swap-base-in`, {
+        computeUnitPriceMicroLamports: String(data.data.default.h),
+        swapResponse,
+        txVersion: 'V0',
+        wallet: owner.publicKey.toBase58(),
+        wrapSol: true,
+        unwrapSol: false,
+    })
+
+    console.log("Swap Transactions Data:", swapTransactions);
+
+    const ata = await getAssociatedTokenAddress(new PublicKey(tokenAddress), owner.publicKey);
+
+    console.log({
+        computeUnitPriceMicroLamports: String(data.data.default.h),
+        swapResponse,
+        txVersion: 'V0',
+        wallet: owner.publicKey.toBase58(),
+        wrapSol: true,
+        unwrapSol: false,
+        // outputMint: ata.toBase58()
+    })
+    console.log(swapTransactions)
+    const allTxBuf = swapTransactions.data.map((tx) => Buffer.from(tx.transaction, 'base64'))
+    const allTransactions = allTxBuf.map((txBuf) =>
+      isV0Tx ? VersionedTransaction.deserialize(txBuf) : Transaction.from(txBuf)
+    )
+
+    let idx = 0
+    for (const tx of allTransactions) {
+        idx++
+        const transaction = tx as VersionedTransaction
+        transaction.sign([owner])
+
+        const txId = await connection.sendTransaction(tx as VersionedTransaction, { skipPreflight: true })
+        console.log("after sending txn");
+        const { lastValidBlockHeight, blockhash } = await connection.getLatestBlockhash({
+          commitment: 'finalized',
+        })
+        console.log(`${idx} transaction sending..., txId: ${txId}`)
+        await connection.confirmTransaction(
+          {
+            blockhash,
+            lastValidBlockHeight,
+            signature: txId,
+          },
+          'confirmed'
+        )
+        console.log(`${idx} transaction confirmed`)
+    }
+
 }
